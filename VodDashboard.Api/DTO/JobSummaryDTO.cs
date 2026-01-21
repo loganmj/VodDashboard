0d6ef9d708e92fae56aa27503059fcf53d5c38a5
loganmj <michaellogan7860@gmail.com>
2026-01-21 15:16:13 -0600

    Added JobSummaryDTO

---
+++ VodDashboard.Api/DTO/JobSummaryDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace VodDashboard.Api.DTO
+{
+    public record JobSummaryDTO
+    {
+        public required string Id { get; set; };
+        public bool HasCleanVideo { get; set; }
+        public int HighlightCount { get; set; }
+        public int SceneCount { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
